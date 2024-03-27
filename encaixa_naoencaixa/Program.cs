76a6df3e8467a8f4f9e413f4d85079abd130cfbc
ALUNO <ALUNO@LABFPC22>
2024-03-27 11:53:40 -0300

    Adicionar arquivos de projeto.

---
+++ encaixa_naoencaixa/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace encaixa_naoencaixa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < a; i++)
+            {
+                string[] x = Console.ReadLine().Split(' ');
+                string s = x[0];
+                string v = x[1];
+                bool res = s.EndsWith(v);
+                if ( res )
+                {
+                    Console.WriteLine("encaixa");
+                }
+                else { Console.WriteLine("nao encaixa"); }
+
+
+
+
+
+            }
+        }
+    }
+}
